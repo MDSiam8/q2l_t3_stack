aa44ba19fb87921e409ec5e95d346fde5c471a94
cyu60 <chinatchinat123@gmail.com>
2023-12-20 11:44:19 +0900

    inital implementation with the landing page

---
+++ src/pages/lab/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import Experience from "../../AnalyticalBalanceLab/components/Experience";
+// import Experience from "../../../AnalyticalBalanceLab/components/Experience";
+import * as THREE from "three";
+
+type RootType = ReactDOM.Root | null;
+
+function MyApp(): JSX.Element | null {
+  const [root, setRoot] = useState<RootType>(null);
+
+  useEffect(() => {
+    const rootElement = document.querySelector("#root") as HTMLElement;
+    if (!rootElement) {
+      throw new Error("Couldn't find the root element");
+    }
+
+    if (!root) {
+      const newRoot = ReactDOM.createRoot(rootElement);
+      setRoot(newRoot);
+    }
+  }, [root]);
+
+  useEffect(() => {
+    if (root) {
+      root.render(
+        <>
+          <Experience />
+        </>,
+      );
+    }
+  }, [root]);
+
+  return null;
+}
+
+export default MyApp;
