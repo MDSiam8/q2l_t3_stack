0d56dbab148f01c0299c29729a680bb35667459a
Md Siam <mdsiamms.ca@gmail.com>
2023-12-26 23:57:44 -0500

    updated loading screen

--- src/RotaryEvaporation/components/Experience.tsx
+++ src/RotaryEvaporation/components/Experience.tsx
@@ -122,21 +122,29 @@ export default function Experience() {
   };
 
   const [loadingMessage, setLoadingMessage] = useState("Loading Resources");
+  const [dotCount, setDotCount] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setLoadingMessage((prev) => {
-        const dots = (prev.match(/\./g) || []).length;
-        return `Loading Resources${'.'.repeat((dots + 1) % 4)}`;
-      });
+      setDotCount((prevDotCount) => (prevDotCount + 1) % 4);
     }, 500); // Update every half second
 
     return () => clearInterval(interval);
   }, []);
-  
+
   return (
-    <div style={{ position: "relative", height: "100vh" }}>
-      <Suspense fallback={<div style={loadingStyle}>{loadingMessage}</div>}>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center bg-gradient-to-r from-blue-400 via-cyan-500 to-green-400">
+          <div className="rounded-lg border border-transparent bg-black bg-opacity-30 p-6 shadow-lg backdrop-blur-lg backdrop-filter">
+            <p className="text-lg font-thin text-white">
+              {loadingMessage + ".".repeat(dotCount)}
+            </p>
+          </div>
+        </div>
+      }
+    >
+      <div style={{ position: "relative", height: "100vh" }}>
         <Canvas
           shadows
           camera={{
@@ -228,60 +236,47 @@ export default function Experience() {
             <Step20Conclusion nextButtonRef={nextButtonRef} />
           )}
         </Canvas>
-      </Suspense>
-      <div
-        style={{
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          background: "rgba(0, 0, 0, 0.2)", // Semi-transparent background
-          padding: "20px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          userSelect: "none",
-        }}
-      >
-        <div className="flex items-stretch justify-center">
-          <div className="w-lg rounded-lg bg-gray-700 bg-opacity-80 p-6 text-center backdrop-blur-sm">
-            <h1 className="mb-2 text-lg text-white">{stepData.stepTitle}</h1>
-            <p className="text-white">{stepData.directions}</p>
-            <p className=" pt-2 font-mono text-xs font-extralight text-fuchsia-300">
-              {"user_instructions" in stepData
-                ? stepData.user_instructions
-                : null}
-            </p>
-          </div>
-          <div className="ml-4 flex flex-col justify-between self-stretch">
-            <button
-              onClick={handleNextStep}
-              disabled={currentStep === 21 || nextButtonTempDisabled}
-              className={`mb-0 flex-grow transform rounded-lg bg-gradient-to-r from-blue-400 to-purple-500 px-4 py-2 font-bold text-white transition duration-300 hover:scale-105 ${
-                currentStep === 13 || nextButtonTempDisabled
-                  ? "cursor-not-allowed bg-gray-400 opacity-50"
-                  : ""
-              }`}
-              ref={nextButtonRef}
-            >
-              Next Step
-            </button>
-            
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: "rgba(0, 0, 0, 0.2)", // Semi-transparent background
+            padding: "20px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            userSelect: "none",
+          }}
+        >
+          <div className="flex items-stretch justify-center">
+            <div className="w-lg rounded-lg bg-gray-700 bg-opacity-80 p-6 text-center backdrop-blur-sm">
+              <h1 className="mb-2 text-lg text-white">{stepData.stepTitle}</h1>
+              <p className="text-white">{stepData.directions}</p>
+              <p className=" pt-2 font-mono text-xs font-extralight text-fuchsia-300">
+                {"user_instructions" in stepData
+                  ? stepData.user_instructions
+                  : null}
+              </p>
+            </div>
+            <div className="ml-4 flex flex-col justify-between self-stretch">
+              <button
+                onClick={handleNextStep}
+                disabled={currentStep === 21 || nextButtonTempDisabled}
+                className={`mb-0 flex-grow transform rounded-lg bg-gradient-to-r from-blue-400 to-purple-500 px-4 py-2 font-bold text-white transition duration-300 hover:scale-105 ${
+                  currentStep === 13 || nextButtonTempDisabled
+                    ? "cursor-not-allowed bg-gray-400 opacity-50"
+                    : ""
+                }`}
+                ref={nextButtonRef}
+              >
+                Next Step
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
-
-const loadingStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '100vh',
-  background: 'linear-gradient(to right, #6dd5ed, #2193b0)',
-  color: 'white',
-  fontSize: '20px',
-  fontFamily: 'Arial, sans-serif',
-  fontWeight: 'lighter',
-};
