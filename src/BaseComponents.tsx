f7aae3387f75f2947cfe3a3e50fab783c8a732f7
Md Siam <mdsiamms.ca@gmail.com>
2024-02-10 14:11:32 -0500

    created step props

--- src/BaseComponents.tsx
+++ src/BaseComponents.tsx
@@ -1,4 +1,5 @@
 import { GroupProps, ThreeEvent } from "@react-three/fiber";
+import { Object3D } from "three";
 
 // Use the following interface to generate lab components for a lab that is based on ______.
 export interface Props extends GroupProps {
@@ -6,4 +7,10 @@ export interface Props extends GroupProps {
   animationHasAlreadyPlayed?: boolean;
   onClick?: ((event: ThreeEvent<MouseEvent>) => void) | undefined;
   // Add more props here 
+}
+
+export interface StepProps {
+  nextButtonRef: React.RefObject<HTMLButtonElement>; // pass in the next step button reference
+  // maybe look into a previous step button?
+  prevButtonRef?: React.RefObject<HTMLButtonElement>; // pass in the previous step button reference
 }
