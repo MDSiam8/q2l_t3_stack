2ec959d6a27656aee7079a66b1d74da253eb8d8d
Mav <mespin11@jh.edu>
2024-02-11 07:30:57 -0500

    finished dashboard

--- src/pages/dashboard/Sidebar.tsx
+++ src/pages/dashboard/Sidebar.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-// import './styles/globals.css';
+import { BiHomeCircle, BiBookContent, BiUserCircle, BiInfoCircle, BiBell, BiCog } from 'react-icons/bi';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      {/* Add your sidebar content here */}
-      {/* <button className="sidebar-button">Class Information</button>
-      <button className="sidebar-button">Notifications</button>
-      <button className="sidebar-button">Profile</button>
-      <button className="sidebar-button">Settings</button> */}
+      <div className="sidebar-title">Dashboard</div>
+      <div className="sidebar-elements">
+        <div>
+          <BiInfoCircle style={{ marginRight: '10px' }} />
+          Class Information
+        </div>
+        <div>
+          <BiBell style={{ marginRight: '10px' }} />
+          Notifications
+        </div>
+        <div>
+          <BiUserCircle style={{ marginRight: '10px' }} />
+          Profile
+        </div>
+        <div>
+          <BiCog style={{ marginRight: '10px' }} />
+          Settings
+        </div>
+      </div>
     </div>
   );
 }
