624c902e988a96ef2fce388175c0acc19a48b119
Aryavrat77 <95258306+Aryavrat77@users.noreply.github.com>
2024-02-13 21:24:55 -0500

    Dashboard sidebar

--- src/pages/dashboard/Sidebar.tsx
+++ src/pages/dashboard/Sidebar.tsx
@@ -4,7 +4,33 @@ import React from 'react';
 function Sidebar() {
   return (
     <div className="sidebar">
-      {/* Add your sidebar content here */}
+      <div className="sidebar-title">General Physics Lab</div>
+      <div className="sidebar-elements">
+        <div>
+          <img src="dashboard-icon.png"></img>
+          Dashboard
+        </div>
+        <div>
+          <img src="class-info-icon.png"></img>
+          Class Information
+        </div>
+        <div>
+          <img src="notifications-icon.png"></img>
+          Notifications
+        </div>
+        <div>
+          <img src="profile-icon.png"></img>
+          Profile
+        </div>
+        <div>
+          <img src="settings-icon.png"></img>
+          Settings
+        </div>
+        <div>
+          <img src="sign-out-icon.png"></img>
+          Sign out
+        </div>
+      </div>
     </div>
   );
 }
