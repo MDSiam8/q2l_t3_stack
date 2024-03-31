8e53bf53c14f7c9b0c781dc363ec660e306be7bd
Mav <mespin11@jh.edu>
2024-03-31 19:17:34 -0400

    practice

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -3,18 +3,28 @@ import { Button } from "../../components/ui/button";
 import { api } from '~/utils/api'
 
 export default function Dashboard() {
-    const { mutate } = api.logger.create.useMutation();
+    const { mutate } = api.post.create.useMutation();
+
+    const { } = api.post.getLatest.useQuery();
     
     const handleClick = () => {
-        mutate({ user: "Jeffery", activity: "test!"})
+        mutate({ name: "Jeffery"})
     }
 
+    const handleClick2 = () => {
+        
+    }
   return (
     <div className="relative"> {/*Relative H-full not filling the entire page for some reason */}
         <main>
           <div className="main-content">
             <Button onClick={handleClick}>
                 Click
+            </Button>
+          </div>
+          <div className="main-content">
+            <Button onClick={handleClick2}>
+                Click 2
             </Button>
           </div>
         </main>
