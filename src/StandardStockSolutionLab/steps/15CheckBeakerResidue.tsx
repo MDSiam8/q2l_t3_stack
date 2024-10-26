6ef98b3914e8e1c5d72ee1d62ba6ca49d663f3d4
Rich Kim <r.c.kim3@gmail.com>
2024-10-25 20:19:46 -0400

    cleaner implementation of residue at bottom of flask. beaker and flask y axis changes

--- src/StandardStockSolutionLab/steps/15CheckBeakerResidue.tsx
+++ src/StandardStockSolutionLab/steps/15CheckBeakerResidue.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect, forwardRef } from "react";
-import { Html } from "@react-three/drei";
+import { Sphere } from "@react-three/drei";
 import { Beaker } from "../models/Beaker";
 import { Flask } from "../models/Flask";
 import { GlassRod } from "../models/GlassRod";
-import { Group } from "three";
+import { Group, Mesh } from "three";
 
 interface CheckBeakerResidueProps {
   nextButtonRef: React.RefObject<HTMLButtonElement>;
@@ -14,6 +14,7 @@ const Step15CheckBeakerResidue = forwardRef<Group, CheckBeakerResidueProps>(
     const glassRodRef = useRef<THREE.Object3D>(null);
     const beakerRef = useRef<THREE.Object3D>(null);
     const flaskRef = useRef<THREE.Object3D>(null);
+    const groupSphereRef = useRef<Group>(null);
 
     useEffect(() => {
 
@@ -21,22 +22,34 @@ const Step15CheckBeakerResidue = forwardRef<Group, CheckBeakerResidueProps>(
 
     return (
       <group ref={ref}>
-        <Beaker
-          ref={beakerRef}
-          position={[1.8, 5, 2.5]}
-          rotation-y={ 3.14 }
-        />
+        <group>
+          <Beaker
+            ref={beakerRef}
+            position={[0, 4.95, 2.5]}
+            rotation-y={ 3.14 }
+          />
+          <group ref={groupSphereRef}>
+            <Sphere position={[0, 4.8, -1.5]} scale={.3} />
+            {/* <Sphere position={[0.1, 5.2, -1.5]} scale={0.03} />
+            <Sphere position={[-0.12, 5.3, -1.7]} scale={0.03} />
+            <Sphere position={[0.17, 5.1, -1.6]} scale={0.03} />
+            <Sphere position={[0.1, 5.1, -1.5]} scale={0.03} />
+            <Sphere position={[-0.15, 5.2, -1.7]} scale={0.03} />
+            <Sphere position={[0, 5.1, -1.6]} scale={0.03} /> */}
+          </group>
+        </group>
+        
 
         <Flask
           ref={flaskRef}
-          position={[2, 5, -1.5]}
+          position={[0, 4.95, -1.5]}
           rotation-y={(-Math.PI / 180) * 45}
         />
 
         <group rotation={[Math.PI / 36, 0, 0]} position={[0, 6, -1.5]}>
           <GlassRod
             ref={glassRodRef}
-            position={[2, 0, 0]}
+            position={[0, 0, 0]}
           />
         </group>
       </group>
