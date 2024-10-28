3e08c20cece40d7056362be9777188b8f34557b3
Xiyan Zhang <xiyanzhang2020@gmail.com>
2024-10-28 12:11:43 -0400

    added plane geometry

--- src/StandardStockSolutionLab/steps/14TransferSolutionToFlask.tsx
+++ src/StandardStockSolutionLab/steps/14TransferSolutionToFlask.tsx
@@ -4,6 +4,7 @@ import { GlassRod } from "../models/GlassRod";
 import { DistilledWater } from "../models/DistilledWater";
 import { HundredMLFlaskWithFillAnimation } from "../models/HundredMLFlaskWithFillAnimation";
 import { setNextDisabled, setNextEnabled } from "../Experience";
+import * as THREE from "three";
 
 interface Step14Props {
   nextButtonRef: React.RefObject<HTMLButtonElement>;
@@ -34,8 +35,27 @@ const Step14TransferSolution = forwardRef<THREE.Group, Step14Props>(
           onClick={onBeakerClick}
         />
 
-        <GlassRod position={[0, 5.1, -1]} />
+        <group
+          position={[0.8, 5, -1]}
+          rotation={[Math.PI / 2, 0, Math.PI / 2]}
+        >
+          <GlassRod/>
+        </group>
+        
         <DistilledWater position={[0, 5.5, 2.5]} />
+
+        <mesh
+          position={[0.2, 5, -1]} 
+          rotation={[-Math.PI / 2, 0, 0]}
+        >
+        <planeGeometry args={[2, 1]} />
+        <meshBasicMaterial
+          color={0xffffff} 
+          transparent
+          side={THREE.DoubleSide} 
+          depthWrite={false} 
+          />
+        </mesh>
 
         <HundredMLFlaskWithFillAnimation
           startAnimationDelay={startFlaskAnimation}
