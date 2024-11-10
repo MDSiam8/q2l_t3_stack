55f7fcc642d7b6e9736beecb6c4f65f13d540dbd
IsarUslu <110565260+IsarUslu@users.noreply.github.com>
2024-11-10 11:55:51 -0500

    added dashboard icons

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -122,9 +122,18 @@ const notebook4: Notebook = {
   link: "/micropipette",
   image: "buchner.jpeg",
   completed: "Not Started",
+};
+
+const notebook5: Notebook = {
+  id: "5",
+  name: "Standard Solution",
+  updatedAt: new Date(),
+  link: "/standard_solution_lab",
+  image: "buchner.jpeg",
+  completed: "Not Started",
 }
 
-const access_labs = [notebook, notebook2, notebook3, notebook4]
+const access_labs = [notebook, notebook2, notebook3, notebook4, notebook5]
 
 export default function Dashboard() {
   const [notebooks, setNotebooks] = useState<Notebook[]>([]);
@@ -137,7 +146,7 @@ export default function Dashboard() {
       // setNotebooks(body.notebooks);
       // setIsLoading(false);
       // notebooks will be fetched from the backend in the future
-      setNotebooks([notebook, notebook2, notebook3]);
+      setNotebooks([notebook, notebook2, notebook3, notebook4, notebook5]);
       setIsLoading(false);
     };
     if (typeof window !== "undefined") {
