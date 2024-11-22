08db2cdfdd5b99a8685a686f51abb98f2072aa12
Ihsan Ahmed <31258383+ihsan-ahmed0@users.noreply.github.com>
2024-11-22 07:19:16 -0800

    Added new model for pipette, and completed logic and animations for extracting solution with pipette on step 7 of diluting lab.

--- src/DilutingStandardSolutionLab/steps/07FillThePipette.tsx
+++ src/DilutingStandardSolutionLab/steps/07FillThePipette.tsx
@@ -1,58 +1,82 @@
 import React, { forwardRef, useRef, useState } from "react";
-import { Beaker } from "../models/Beaker";
 import { GlassPipette } from "../models/GlassPipette";
 import { PipetteBulb } from "../models/PipetteBulb";
 import * as THREE from "three";
-import { Html } from "@react-three/drei";
-
-
+import { Html, PerspectiveCamera } from "@react-three/drei";
+import { BeakerStockSolutionFill } from "../models/BeakerWithOrangeFillAnim";
+import { Canvas } from "@react-three/fiber";
 
 const Step7FillThePipette = forwardRef((props, ref) => {
 
   const initialBeakerWaterLevel = 1;
+  const initialPipetteWaterLevel = 0;
 
   const [beakerWaterLevel, setBeakerWaterLevel] = useState(initialBeakerWaterLevel);
-  const [pipetteWaterLevel, setPipetteWaterLevel] = useState(0);
-  const [hasPoured, setHasPoured] = useState(false);
+  const [pipetteWaterLevel, setPipetteWaterLevel] = useState(initialPipetteWaterLevel);
+  const [hasExtracted, setHasExtracted] = useState(false);
 
   const beakerWaterRef = useRef<THREE.Mesh>(null);
-  const pourInterval = useRef<NodeJS.Timeout | null>(null);
+  const pipetteWaterRef = useRef<THREE.Mesh>(null);
+  const extractionInterval = useRef<NodeJS.Timeout | null>(null);
 
   // Start filling the water
-  const startFillingWater = () => {
-    if (beakerWaterRef.current) {
-      pourInterval.current = setInterval(() => {
+  const startExtractingWater = () => {
+    if (beakerWaterRef.current && pipetteWaterRef.current) {
+      extractionInterval.current = setInterval(() => {
+        // Lower water level in the beaker
         setBeakerWaterLevel((prevWaterLevel) => {
           const newLevel = prevWaterLevel - 0.0125;
-          if (newLevel <= 1) {
+          if (newLevel <= 0) {
+            beakerWaterRef.current!.scale.set(1, 0, 1);
+            const newVerticalPos = beakerWaterRef.current!.position.y - ((prevWaterLevel * 0.7) * 0.5);
+            beakerWaterRef.current!.position.set(0, newVerticalPos, 0.58); // Anchor to bottom of flask
+            return 0;
+          } else if (newLevel <= 1) {
             beakerWaterRef.current!.scale.set(1, newLevel, 1);
-            const newVerticalPos = beakerWaterRef.current.position.y - ((prevWaterLevel * 0. - newLevel * 0.7) * 0.5);
-            beakerWaterRef.current!.position.set(-0.236, newVerticalPos, 0.594); // Anchor to bottom of flask
+            const newVerticalPos = beakerWaterRef.current!.position.y - ((prevWaterLevel * 0.7 - newLevel * 0.7) * 0.5);
+            beakerWaterRef.current!.position.set(0, newVerticalPos, 0.58); // Anchor to bottom of flask
             return newLevel;
           } else {
-            clearInterval(pourInterval.current!);
+            clearInterval(extractionInterval.current!);
             return prevWaterLevel;
           }
         });
+
+        // Raise water level in the pipette
         setPipetteWaterLevel((prevWaterLevel) => {
-          return prevWaterLevel;
+          const newLevel = prevWaterLevel + 0.05;
+          if (newLevel >= 1.25) {
+            pipetteWaterRef.current!.scale.set(1, 1.25, 1);
+            const newVerticalPos = pipetteWaterRef.current!.position.y - ((prevWaterLevel * 2) * 0.5);
+            pipetteWaterRef.current!.position.set(0, newVerticalPos, 0.58);
+            return 1.25;
+          } else if (newLevel < 1.25) {
+            pipetteWaterRef.current!.scale.set(1, newLevel, 1);
+            const newVerticalPos = pipetteWaterRef.current!.position.y - ((prevWaterLevel * 2 - newLevel * 2) * 0.5);
+            pipetteWaterRef.current!.position.set(0, newVerticalPos, 0.58);
+            return newLevel;
+          } else {
+            clearInterval(extractionInterval.current!);
+            return prevWaterLevel;
+          }
         });
+
       }, 100); // Adjust the water level every 100ms
     }
   };
 
   // Stop filling the water and validate the level
-  const stopFillingWater = () => {
-    if (pourInterval.current) {
-      clearInterval(pourInterval.current);
+  const stopExtractingWater = () => {
+    if (extractionInterval.current) {
+      clearInterval(extractionInterval.current);
     }
 
-    if (beakerWaterLevel >= 0.61 && beakerWaterLevel <= 0.63) {
-      console.log(beakerWaterLevel);
+    if (pipetteWaterLevel >= 0.9 && pipetteWaterLevel <= 1.1) {
+      console.log(pipetteWaterLevel);
       alert("Success! You filled the water correctly.");
-      setHasPoured(true);
-    } else if (beakerWaterLevel > 0.63) {
-      console.log(beakerWaterLevel);
+      setHasExtracted(true);
+    } else if (pipetteWaterLevel > 1.1) {
+      console.log(pipetteWaterLevel);
       alert(`Try again! You "overfilled" the water.`);
       resetWater();
     }
@@ -60,12 +84,19 @@ const Step7FillThePipette = forwardRef((props, ref) => {
 
   // Reset the water level
   const resetWater = () => {
+
     setBeakerWaterLevel(initialBeakerWaterLevel);
     if (beakerWaterRef.current) {
       beakerWaterRef.current!.scale.set(1, initialBeakerWaterLevel, 1);
-      beakerWaterRef.current!.position.set(-0.236, 5.4, 0.594);
+      beakerWaterRef.current!.position.set(0, 5.4, 0.58);
     }
-    setHasPoured(false);
+
+    setPipetteWaterLevel(initialPipetteWaterLevel);
+    if (pipetteWaterRef.current) {
+      pipetteWaterRef.current!.scale.set(1, initialPipetteWaterLevel, 1);
+      pipetteWaterRef.current!.position.set(0, 5.55, 0.58);
+    }
+    setHasExtracted(false);
     /*
     if (nextButtonRef.current) {
       setNextDisabled(nextButtonRef);
@@ -74,41 +105,67 @@ const Step7FillThePipette = forwardRef((props, ref) => {
   };
 
   // Handle the main pour action
-  const handlePour = () => {
-    startFillingWater(); // Start the water filling animation
+  const handleExtraction = () => {
+    startExtractingWater(); // Start the water filling animation
   };
 
   return (
     <group>
       <GlassPipette position={[0, 5.5, 0.58]} />
       <PipetteBulb position={[0, 8, 0.58]} />
-      <Beaker position={[0, 4.96, 0]} />
+      {/*<Beaker position={[0, 4.96, 0]} />*/}
+      <BeakerStockSolutionFill position={[0, 4.96, 0.58]} />
 
-      {/* Water Mesh */}
+      {/* Water Mesh inside the Beaker */}
       <mesh
-          ref={beakerWaterRef}
-          position={[-0.236, 5.4, 0.594]}
-          scale={[1, initialBeakerWaterLevel, 1]} // Dynamic height based on fill
-        >
-          <cylinderGeometry args={[0.35, 0.35, 0.7, 32]} />
-          <meshStandardMaterial color={"lightblue"} transparent opacity={0.6} />
+        ref={beakerWaterRef}
+        position={[0, 5.4, 0.58]}
+        scale={[1, beakerWaterLevel, 1]} // Dynamic height based on fill
+      >
+        <cylinderGeometry args={[0.35, 0.35, 0.7, 32]} />
+        <meshStandardMaterial color={"lightblue"} transparent opacity={0.6} />
       </mesh>
 
-       {/* Pour Button */}
-       <Html
-          position={[0, 8, -1.0]}
-          transform
-          scale={0.5}
-          rotation={[0, Math.PI / 2, 0]} // Corrected rotation
+      {/* Water Mesh inside the Pipette */}
+      <mesh
+        ref={pipetteWaterRef}
+        position={[0, 5.55, 0.58]}
+        scale={[1, pipetteWaterLevel, 1]} // Dynamic height based on fill
+      >
+        <cylinderGeometry args={[0.017, 0.017, 2, 32]} />
+        <meshStandardMaterial color={ new THREE.Color(0x74cdee) } transparent opacity={0.6} />
+      </mesh>
+
+      {/* Pour Button */}
+      <Html
+        position={[0, 8, -1.0]}
+        transform
+        scale={0.5}
+        rotation={[0, Math.PI / 2, 0]} // Corrected rotation
+      >
+        <button
+          onMouseDown={handleExtraction} // Start pouring on mouse down
+          onMouseUp={stopExtractingWater} // Stop pouring on mouse up
+          className="rounded-md p-3 text-sm font-bold text-white bg-blue-500 hover:bg-blue-600 shadow-lg transition-transform duration-300"
         >
-          <button
-            onMouseDown={handlePour} // Start pouring on mouse down
-            onMouseUp={stopFillingWater} // Stop pouring on mouse up
-            className="rounded-md p-3 text-sm font-bold text-white bg-blue-500 hover:bg-blue-600 shadow-lg transition-transform duration-300"
-          >
-            Extract Solution
-          </button>
-        </Html>
+          Extract Solution
+        </button>
+      </Html>
+
+      {/* Canvas with zoomed in view */}
+      <Html position={[0, 6.5, -1.2]} transform scale={0.8} rotation-y={Math.PI / 2}>
+        <div style={{ width: 100, height: 70, border: "1px solid black" }}>
+          <Canvas>
+            <PerspectiveCamera
+            makeDefault
+            position={ [0, 0, 0] }
+            onUpdate={self => self.lookAt(0, 6, -1)}
+            fov={ 20 }
+            />
+            <ambientLight intensity={0.5} />
+          </Canvas>
+        </div>
+      </Html>
     </group>
   );
 });
