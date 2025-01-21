916574ea713f9e840d4aed7ae6e132b4a88076ab
Alina Pan <alinapanyue@outlook.com>
2025-01-21 17:16:09 -0500

    changed multiple choice questions

--- src/SpecLab/steps/12AnalyzingCalibrationCurve.tsx
+++ src/SpecLab/steps/12AnalyzingCalibrationCurve.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useRef, useState, useEffect } from "react";
 import * as THREE from "three";
-import graph01 from '../images/graph_01.png'; 
+import graph01 from '../images/graph_01.png';
 import AnswerBox from "../ui_overlay/AnswerBox";
 interface StopperRef {
   replayAnimation: () => void;
@@ -15,58 +15,49 @@ const Step12PrepareAdditionalDilutions = forwardRef<{}, Step12ComponentProps>(
   const stopperRef = useRef<StopperRef>(null);
   const questions = [
     {
-      question: "Why is it important to pour the standard solution into a clean beaker before pipetting?",
+      question: "Given an absorbance of 0.07, what is the concentration (ppm) according to the graph?",
       choices: [
-        { id: "a", text: "To prevent contamination from the stock bottle" },
-        { id: "b", text: "To measure the volume more accurately" },
-        { id: "c", text: "It's not necessary; you can pipette directly from the volumetric flask" }
-      ],
-      correctAnswers: "a",
-    },
-    {
-      question: "Which pipette is most suitable for transferring 7 mL of solution?",
-      choices: [
-        { id: "a", text: "Micropipette" },
-        { id: "b", text: "5 mL glass pipette" },
-        { id: "c", text: "10 mL glass pipette" }
-      ],
-      correctAnswers: "c",
-    },
-    {
-      question: "At which point should the bottom of the meniscus align when measuring liquid in a pipette?",
-      choices: [
-        { id: "a", text: "Above the calibration mark" },
-        { id: "b", text: "At the calibration mark" },
-        { id: "c", text: "beliw the calibration mark" }
+        { id: "a", text: "0.153 ppm" },
+        { id: "b", text: "0.1 ppm" },
+        { id: "c", text: "0.2 ppm" }
       ],
       correctAnswers: "b",
     },
     {
-      question: "Why should you allow the pipette to drain completely without blowing out the remaining liquid?",
+      question: "Given an absorbance of 0.153, what is the concentration (ppm) according to the graph?",
       choices: [
-        { id: "a", text: "To ensure all liquid is transferred" },
-        { id: "b", text: "Blowing out can introduce air bubbles, affecting volume accuracy" },
-        { id: "c", text: "It's faster to let it drain naturally" }
+        { id: "a", text: "0.1 ppm" },
+        { id: "b", text: "0.6 ppm" },
+        { id: "c", text: "0.2 ppm" }
+      ],
+      correctAnswers: "c",
+    },
+    {
+      question: "Given an absorbance of 0.315, what is the concentration (ppm) according to the graph?",
+      choices: [
+        { id: "a", text: "0.315 ppm" },
+        { id: "b", text: "0.4 ppm" },
+        { id: "c", text: "0.07 ppm" }
       ],
       correctAnswers: "b",
     },
     {
-      question: "What's the best method to ensure the volumetric flask is filled exactly to the calibration mark?",
+      question: "Given an absorbance of 0.472, what is the concentration (ppm) according to the graph?",
       choices: [
-        { id: "a", text: "Fill to just below the mark, then use a dropper to add water dropwise" },
-        { id: "b", text: "Pour distilled water quickly until it reaches the mark" },
-        { id: "c", text: "Overfill the flask and remove excess water" }
+        { id: "a", text: "0.6 ppm" },
+        { id: "b", text: "0.4 ppm" },
+        { id: "c", text: "0.8 ppm" }
       ],
       correctAnswers: "a",
     },
     {
-      question: "What's the proper way to mix the solution in a volumetric flask?",
+      question: "Given an absorbance of 0.63, what is the concentration (ppm) according to the graph?",
       choices: [
-        { id: "a", text: "Shake it vigorously up and down" },
-        { id: "b", text: "Stir the solution with a glass rod" },
-        { id: "c", text: "Gently invert the flask several times" }
+        { id: "a", text: "0.8 ppm" },
+        { id: "b", text: "0.6 ppm" },
+        { id: "c", text: "0.4 ppm" }
       ],
-      correctAnswers: "c",
+      correctAnswers: "a",
     },
   ]
   const [isCorrect, setIsCorrect] = useState(false);
@@ -87,16 +78,16 @@ const Step12PrepareAdditionalDilutions = forwardRef<{}, Step12ComponentProps>(
     }, 5000); // Adjust duration to match the actual animation time
   };
   // useEffect(() => {
-  //   updateBalanceReadingAfterAddingPowder(0.5017);
-  //   if (nextButtonRef && nextButtonRef.current) {
-  //     nextButtonRef.current.disabled = true; // Disable the button initially
-  //     nextButtonRef.current.style.opacity = "0.5";
-  //   }
+  //  updateBalanceReadingAfterAddingPowder(0.5017);
+  //  if (nextButtonRef && nextButtonRef.current) {
+  //    nextButtonRef.current.disabled = true; // Disable the button initially
+  //    nextButtonRef.current.style.opacity = "0.5";
+  //  }
   // }, []);
   // const updateBalanceReadingAfterAddingPowder = (num: number) => {
-  //   if (balanceWithAnimationsRef.current) {
-  //     balanceWithAnimationsRef.current.updateBalanceReading(num);
-  //   }
+  //  if (balanceWithAnimationsRef.current) {
+  //    balanceWithAnimationsRef.current.updateBalanceReading(num);
+  //  }
   // };
   return (
     <group>
